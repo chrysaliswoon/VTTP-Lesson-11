843d911046dcea68609fde63ed18807dda21b591
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2022-07-18 13:51:07 +0800

    Created workshop folder

--- day11workshop/src/main/java/sg/edu/nus/iss/day11workshop/Day11workshopApplication.java
+++ day11workshop/src/main/java/sg/edu/nus/iss/day11workshop/Day11workshopApplication.java
@@ -2,12 +2,28 @@ package sg.edu.nus.iss.day11workshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.DefaultApplicationArguments;
+
+import java.util.Collections;
+
+
 
 @SpringBootApplication
 public class Day11workshopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Day11workshopApplication.class, args);
+		SpringApplication app = new SpringApplication(Day11workshopApplication.class);
+		String PORT = "3000";
+		ApplicationArguments cliOpts = new DefaultApplicationArguments(args);
+
+		if (cliOpts.containsOption("port")) {
+			PORT = cliOpts.getOptionValues("port").get(0);
+		}
+
+		app.setDefaultProperties(Collections.singletonMap("server.port", PORT));
+		app.run(args);
+
 	}
 
 }
