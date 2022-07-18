ee8a2cbd23824eeecebb321349384f865ef36616
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2022-07-18 12:19:16 +0800

    Updated files

--- day11lecture/src/main/java/sg/edu/nus/iss/day11lecture/Day11lectureApplication.java
+++ day11lecture/src/main/java/sg/edu/nus/iss/day11lecture/Day11lectureApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.DefaultApplicationArguments;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 import java.util.Collections;
 
@@ -11,14 +13,18 @@ import java.util.Collections;
 public class Day11lectureApplication {
 
 	public static void main(String[] args) {
-		// SpringApplication.run(Day11lectureApplication.class, args); //? Starts the application
+		// SpringApplication.run(Day11lectureApplication.class, args); //? Starts the
+		// application
 
-		SpringApplication app = new SpringApplication(Day11lectureApplication.class); //? Creates a new variable called app that starts the Springboot application
+		SpringApplication app = new SpringApplication(Day11lectureApplication.class); // ? Creates a new variable called
+																						// app that starts the
+																						// Springboot application
 
-		String PORT = "8080"; //? Sets the default port
+		String PORT = "8080"; // ? Sets the default port
 		ApplicationArguments cliOpts = new DefaultApplicationArguments(args);
 
-		if (cliOpts.containsOption("port")) { //? If the terminal command contains port, get the value after the string "port" and change the default port to that value
+		if (cliOpts.containsOption("port")) { // ? If the terminal command contains port, get the value after the string
+												// "port" and change the default port to that value
 			PORT = cliOpts.getOptionValues("port").get(0);
 		}
 
@@ -26,6 +32,14 @@ public class Day11lectureApplication {
 		System.out.printf("PORT started on " + PORT);
 		app.run(args);
 
+	}
+
+	@Bean
+	public CommonsRequestLoggingFilter log() {
+		CommonRequestLoggingFilter logger = new CommonRequestLoggingFilter();
+		logger.setIncludeClientInfo(true);
+		logger.setIncludeQueryString(true);
+		return logger;
 	}
 
 }
